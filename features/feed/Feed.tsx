f4012f0271f519ad07b8c0fab4fdf2e15ee3be19
Nimesh Maharjan <nimesh.maharjan>
2022-09-13 12:50:08 +0545

    addition of recoil and modals for adding post in feed section

--- features/feed/Feed.tsx
+++ features/feed/Feed.tsx
@@ -1,6 +1,10 @@
-import Input from "@components/ui/Input";
+import Input from "@features/feed/components/Input";
 
 const Feed = () => {
-  return <section className="space-y-6 pb-24 max-w-lg"></section>;
+  return (
+    <section className="space-y-6 pb-24 max-w-lg">
+      <Input></Input>
+    </section>
+  );
 };
 export default Feed;
